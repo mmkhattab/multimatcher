c16ecf91344272383085fb03c64933396cabc855
Muhammad Khattab <khattab.m.muhammad@gmail.com>
2023-11-04 21:42:07 +0100

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='multimatcher',
-    version='0.0.2',
+    version='0.0.3',
     install_requires=[
         'requests',
         'importlib-metadata; python_version >= "3.8"',
